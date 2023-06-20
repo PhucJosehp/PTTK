98a42904a57b82bf04d01e9e16a54a091b02f008
Josehp Tran <tmphuc0805@gmail.com>
2023-06-20 11:26:58 +0700

    add tour

--- frontend/src/screens/Tours.js
+++ frontend/src/screens/Tours.js
@@ -1,12 +1,38 @@
 import React from "react";
 import "../scss/screen.scss";
 import Sidebar from "../components/Sidebar";
+import Searchbar from "../components/Searchbar";
+import Showinf from "../components/Showinf";
+import add from "../assets/icon/Add.png";
 
 const Tours = () => {
   return (
-    <div>
+    <div className="services">
       <Sidebar />
-      Tours
+      <div className="services__view">
+        <Showinf name="TOURS" />
+        <div className="services__view__search-area">
+          <Searchbar name="Customer ID" />
+          <Searchbar name="Tour ID" />
+          <Searchbar name="Date" />
+          <img
+            src={add}
+            alt="addi-icon"
+            className="services__view__search-area__icon icon"
+          />
+        </div>
+
+        <div className="services__view__showdata">
+          <ul className="services__view__showdata__title">
+            <li style={{ marginLeft: -60 }}>Customer ID</li>
+            <li style={{ marginLeft: -100 }}>Tour ID</li>
+            <li style={{ marginLeft: -70 }}>Tour Name</li>
+            <li style={{ marginLeft: 40 }}>Number of people</li>
+            <li>Price</li>
+            <li>Date</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
