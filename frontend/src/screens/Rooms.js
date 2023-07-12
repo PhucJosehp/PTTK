9de04bc2ef3ac058179b219a3386ec362e535b6b
Josehp Tran <tmphuc0805@gmail.com>
2023-07-12 20:18:45 +0700

    add

--- frontend/src/screens/Rooms.js
+++ frontend/src/screens/Rooms.js
@@ -1,14 +1,35 @@
-import React from "react";
+import { React, useState } from "react";
 import "../scss/screen.scss";
 import Sidebar from "../components/Sidebar";
 import Showinf from "../components/Showinf";
+import Searchbar from "../components/Searchbar";
 
 const Rooms = () => {
+  const [records, setRecords] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+  const getDataSearch = (data) => {
+    setRecords(data);
+  };
   return (
-    <div className="dashboard">
+    <div className="room">
       <Sidebar />
-      <div className="dashboard__view">
+      <div className="room__view">
         <Showinf name="ROOM" />
+        <div className="room__view__search-area">
+          <Searchbar
+            name="Customer ID"
+            search="MaKH"
+            onChange={getDataSearch}
+          />
+          <Searchbar
+            name="Customer name"
+            search="HoTen"
+            onChange={getDataSearch}
+          />
+        </div>
+
+        <div className="room__view__showroom"></div>
       </div>
     </div>
   );
